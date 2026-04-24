c3de04d00b1489b2d3bb23428fe3fd9ba850afd8
jojo <jojositohangg@gmail.com>
2026-04-24 09:22:30 +0700

    Upload Github pertamaku

---
+++ data.js
@@ -0,0 +1,11 @@
+const databaseProyek = {
+    "": {
+        judul: "",
+        keahlian: [],
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJLZV8z_UabnTjgrA7QsFBxM-JWjgrHfcmyw&s",
+        latar: "",
+        tantangan: "",
+        pelajaran: "",
+        link: ""
+    },
+};
